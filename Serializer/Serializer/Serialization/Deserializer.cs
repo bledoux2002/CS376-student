b031b2a5dd34e2076497d0a2fb748fddef183d87
Benjamin Ledoux <bledoux2002@gmail.com>
2025-10-07 17:23:05 -0500

    assignment 1

--- Serializer/Serializer/Serialization/Deserializer.cs
+++ Serializer/Serializer/Serialization/Deserializer.cs
@@ -284,7 +284,11 @@ namespace UnityEngine
             SkipWhitespace();
 
             // You've got the id # of the object.  Are we done now?
-            throw new NotImplementedException("Fill me in");
+                // If it's in the idTable already, yes.
+            if (idTable.ContainsKey(id))
+            {
+                return idTable[id];
+            }
 
             // Assuming we aren't done, let's check to make sure there's a { next
             SkipWhitespace();
@@ -306,14 +310,17 @@ namespace UnityEngine
                     $"Expected a type name (a string) in 'type: ...' expression for object id {id}, but instead got {typeName}");
 
             // Great!  Now what?
-            throw new NotImplementedException("Fill me in");
+                // Create object and add to idTable
+            var o = Utilities.MakeInstance(type);
+            idTable[id] = o;
 
             // Read the fields until we run out of them
             while (!End && PeekChar != '}')
             {
                 var (field, value) = ReadField(id);
-                // We've got a field and a value.  Now what?
-                throw new NotImplementedException("Fill me in");
+                // We've got a field and a value.  Now what?.
+                    // Create and populate fields in object
+                Utilities.SetFieldByName(o, field, value);
             }
 
             if (End)
@@ -322,7 +329,8 @@ namespace UnityEngine
             GetChar();  // Swallow close bracket
 
             // We're done.  Now what?
-            throw new NotImplementedException("Fill me in");
+                // GIMME THE OBJECT
+            return o;
         }
 
     }
--- Serializer/Serializer/Serialization/Serializer.cs
+++ Serializer/Serializer/Serialization/Serializer.cs
@@ -190,28 +190,28 @@ namespace UnityEngine
             switch (o)
             {
                 case null:
-                    throw new NotImplementedException("Fill me in");
+                    Write("null");
                     break;
 
                 case int i:
-                    throw new NotImplementedException("Fill me in");
+                    Write(i);
                     break;
 
                 case float f:
-                    throw new NotImplementedException("Fill me in");
+                    Write(f);
                     break;
 
-                // Not: don't worry about handling strings that contain quote marks
+                // Note: don't worry about handling strings that contain quote marks
                 case string s:
-                    throw new NotImplementedException("Fill me in");
+                    Write("\"" + s + "\"");
                     break;
 
                 case bool b:
-                    throw new NotImplementedException("Fill me in");
+                    Write(b);
                     break;
 
                 case IList list:
-                    throw new NotImplementedException("Fill me in");
+                    WriteList(list);
                     break;
 
                 default:
@@ -231,7 +231,24 @@ namespace UnityEngine
         /// <param name="o">Object to serialize</param>
         private void WriteComplexObject(object o)
         {
-            throw new NotImplementedException("Fill me in");
+            var (id, isNew) = GetId(o);
+            Write("#" + id);
+            if (!isNew) return;
+
+            WriteBracketedExpression("{", () =>
+            {
+                WriteField("type", o.GetType().Name, true);
+
+                var fields = Utilities.SerializedFields(o);
+                foreach (var kv in fields)
+                {
+                    if (kv.Key == "type")
+                        continue;
+                    WriteField(kv.Key, kv.Value, false);
+                }
+
+            },
+                "}");
         }
     }
 }
