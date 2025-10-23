66bb6bf4a884e5bb002e9488c2b51205c327f36a
Benjamin Ledoux <bledoux2002@gmail.com>
2025-10-22 21:10:23 -0500

    Making an exploding box complete

--- Angry Blox/Assets/Code/Bomb.cs
+++ Angry Blox/Assets/Code/Bomb.cs
@@ -5,6 +5,25 @@ public class Bomb : MonoBehaviour {
     public GameObject ExplosionPrefab;
 
     public void OnCollisionEnter2D(Collision2D collision) {
-        // FILL ME IN
+        foreach (var contact in collision.contacts)
+        {
+            if (contact.normalImpulse > ThresholdImpulse)
+            {
+                Boom();
+            }
+        }
+    }
+
+    private void Destruct()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Boom()
+    {
+        gameObject.GetComponent<PointEffector2D>().enabled = true;
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+        Invoke("Destruct", 0.1f);
     }
 }
